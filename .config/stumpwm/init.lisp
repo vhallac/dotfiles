38a0e5c115e4fca1aa837bf1b75ea5df44bb0f1e
Vedat Hallac <vedat@hallac.net>
2021-04-25 12:03:55 +0300

    stumpwm: Switch to float groups with fullscreen windows
    I am moving away from tiling windows - to try and see if floating windows
    cause less friction with my style.

--- .config/stumpwm/init.lisp
+++ .config/stumpwm/init.lisp
@@ -25,15 +25,45 @@
 
 (split-frame-in-dir (current-group) :column 500/809)
 (add-group (current-screen) "Browse" :background t)
-(add-group (current-screen) "Code" :background t)
 (add-group (current-screen) "Chat" :background t)
+(add-group (current-screen) "Code" :background t)
 (add-group (current-screen) "Temp1" :background t)
 (add-group (current-screen) "Temp2" :background t)
 (add-group (current-screen) "Temp3" :background t)
-(add-group (current-screen) "Float-1" :background t :type 'float-group)
-(add-group (current-screen) "Float-2" :background t :type 'float-group)
+(add-group (current-screen) "Temp4" :background t)
+(add-group (current-screen) "Temp5" :background t)
+;;; This is my default for now
+;;; Related to the Float-Master default, check out the fast group switch keybindings
+(switch-to-group
+ (add-group (current-screen) "Float-Dump" :background t :type 'float-group))
+(add-group (current-screen) "Float-Master" :background t :type 'float-group)
+(add-group (current-screen) "Float-Browse" :background t :type 'float-group)
+(add-group (current-screen) "Float-Chat" :background t :type 'float-group)
+(add-group (current-screen) "Float-Code" :background t :type 'float-group)
+(add-group (current-screen) "Float-Temp1" :background t :type 'float-group)
+(add-group (current-screen) "Float-Temp2" :background t :type 'float-group)
+(add-group (current-screen) "Float-Temp3" :background t :type 'float-group)
+(add-group (current-screen) "Float-Temp4" :background t :type 'float-group)
+(add-group (current-screen) "Float-Temp5" :background t :type 'float-group)
 
 (setf *mouse-focus-policy* :click)
+
+(defvar *vh/fullscreen-rules* '( (:instance "emacs")
+                                (:class "firefox")
+                                (:instance "slack")
+                                (:instance "xterm")
+                                (:class "disable-jetbrains-idea" :type :NORMAL)))
+
+(defun fullscreen-new-window-hook (new-window)
+  (when (and (typep (current-group) 'float-group)
+             (dolist (rule *vh/fullscreen-rules*)
+               (when (apply #'window-matches-properties-p
+                            (cons new-window rule))
+                 (return rule))))
+    (setf (window-fullscreen new-window) t)))
+
+
+(add-hook *new-window-hook* #'fullscreen-new-window-hook)
 
 (define-key *root-map* (kbd "c") "exec /usr/bin/uxterm")
 (define-key *root-map* (kbd "C-c") "exec /usr/bin/uxterm")
@@ -62,27 +92,37 @@
 
 (define-key *top-map* (kbd "s-G") "gselect")
 
-(define-key *top-map* (kbd "s-1") "gselect 1")
-(define-key *top-map* (kbd "s-2") "gselect 2")
-(define-key *top-map* (kbd "s-3") "gselect 3")
-(define-key *top-map* (kbd "s-4") "gselect 4")
-(define-key *top-map* (kbd "s-5") "gselect 5")
-(define-key *top-map* (kbd "s-6") "gselect 6")
-(define-key *top-map* (kbd "s-7") "gselect 7")
-(define-key *top-map* (kbd "s-8") "gselect 8")
-(define-key *top-map* (kbd "s-9") "gselect 9")
+(defvar *vh/active-group-type* :float)
 
-(define-key *top-map* (kbd "s-C-0") "gmove 10")
-(define-key *top-map* (kbd "s-C-1") "gmove 1")
-(define-key *top-map* (kbd "s-C-2") "gmove 2")
-(define-key *top-map* (kbd "s-C-3") "gmove 3")
-(define-key *top-map* (kbd "s-C-4") "gmove 4")
-(define-key *top-map* (kbd "s-C-5") "gmove 5")
-(define-key *top-map* (kbd "s-C-6") "gmove 6")
-(define-key *top-map* (kbd "s-C-7") "gmove 7")
-(define-key *top-map* (kbd "s-C-8") "gmove 8")
-(define-key *top-map* (kbd "s-C-9") "gmove 9")
-(define-key *top-map* (kbd "s-C-0") "gmove 10")
+(defun seq (end &key (start 1))
+  "Generate integer sequence ending at END, and optionally starting at START."
+  (loop for i from start to end collect i))
+
+(defun switch-to-tiled ()
+  "Convert the fast group switch map to tiled groups"
+  (setf *vh/active-group-type* :tiled)
+  (dolist (i (seq 9))
+    (define-key *top-map* (kbd (format nil "s-~a" i)) (format nil "gselect ~a" i))
+    (define-key *top-map* (kbd (format nil "s-C-~a" i)) (format nil "gmove ~a" i))))
+
+(defun switch-to-float ()
+  "Convert the fast group switch map to tiled groups"
+  (setf *vh/active-group-type* :float)
+  (dolist (i (seq 9))
+    (define-key *top-map* (kbd (format nil "s-~d" i)) (format nil "gselect ~d" (+ 10 i)))
+    (define-key *top-map* (kbd (format nil "s-C-~d" i)) (format nil "gmove ~d" (+ 10 i)))))
+
+(defcommand toggle-fast-switch-set () ()
+  "Changes group switch keybindings to either the tiled or the float set."
+  (if (eql *vh/active-group-type* :tiled)
+      (switch-to-float)
+      (switch-to-tiled)))
+
+(switch-to-float)
+
+(define-key *groups-map* (kbd "t") "toggle-fast-switch-set")
+
+(define-key *root-map* (kbd "s-g") "gother")
 
 (define-key *top-map* (kbd "s-D") "exec rofi -show-icons -show drun")
 (define-key *top-map* (kbd "s-d") "exec rofi -show-icons -show run")
